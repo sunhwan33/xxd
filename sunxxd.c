8a87c1a4a4f63e6af09fedfc4bc44e27a29fcb47
sunhwan33 <sunhwan33@gmail.com>
2023-05-08 18:31:14 +0900

    Update sunxxd.c

--- sunxxd.c
+++ sunxxd.c
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-#include <stdio.h>
-
-int main(char * argc, char* argv){
-    FILE * fp;
-    fp = fopen(argv[1], "r");
-    
-=======
 #include <stdio.h>
 #include <ctype.h>
 
 int main(int argc, char **argv) {
-
+    FILE * fp = fopen(argv[1], "r");
+    fseek(fp, 0, SEEK_END) ;
+    size
     return 0 ;
->>>>>>> 271a40459f06be0ae8baf19e6bd7d0d8e94f63c8
 }
